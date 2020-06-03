481bdda20310da6f700a3f3cbdf3dcfc9d76662f
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-03 17:11:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,21 @@
 def prime?(integer)
   if integer >= 1
-    
+    if n == 2:
+      return true
+    if n
   else
     return false
 end 
+
+
+    if n == 2:
+        return True
+    if n == 3:
+        return True
+    if n % 2 == 0:
+        return False
+    if n % 3 == 0:
+        return False
+
+    i = 5
+    w = 2
