ef0ff040870df2cc48c9bde5e4a4657d5298f77e
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-03 18:17:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(number)
   return false if !number.integer?
   return false if number < 2
   return true if number == 2
-  (2..number - 1).each {|int| return false if number % 2 ==0}
+  (2..number - 1).each {|int| return false if number % 2 == 0}
   true
 end 
     
