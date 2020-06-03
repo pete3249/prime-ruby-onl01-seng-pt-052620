ecbe1db65dde35bc428a3e29955517f5cb0ac5a4
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-03 17:22:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,26 +1,22 @@
 def prime?(integer)
   numbers_array = (1..integer).to_a
   numbers_array.each do |number|
-    if number/
-  end 
-  
- 
-    
-  
-    
-    
-    
-    if integer == 2:
+    if number == 1 || number == 2 || number == 3
       return true
-    if integer == 3
-      return true
+    elsif 
+    
+      
+    else 
+       return false
+    end
+end 
+    
+    
+   
+
     if integer % 2 == 0
       return false
     if integer % 
-  else
-    return false
-  end 
-end 
-
+  
 
    
