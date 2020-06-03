9b51c36e8a83a01e7d2566da8389974b49ccbe85
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-03 18:15:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,19 +3,8 @@ def prime?(number)
   return false if number < 2
   return true if number == 2
   (2..number - 1).each {|int| return false if number % 2 ==0}
-  
-  
-  numbers_array = (2..number -1).to_a
-  numbers_array.each do |number|
-    if integer >= -1 && integer <= 1
-      return false
-    elsif integer == 2 || integer == 3
-      return true 
-    else 
-      if integer % 2 
-  end 
+  true
 end 
-
     
     
     
